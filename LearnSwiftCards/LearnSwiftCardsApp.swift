bba6488cf1a2e2eceb3204df8d1bd0276e24b41c
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-05-06 13:50:49 +0300

    first commit

---
+++ LearnSwiftCards/LearnSwiftCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftCardsApp.swift
+//  LearnSwiftCards
+//
+//  Created by Dmitrii Nazarov on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
